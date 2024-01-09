1d24f798b61b68449c849cebef36a33489d80eae
wanderlandeurasiers <jake4kurtz@gmail.com>
2024-01-09 11:39:57 -0700

    update copyright year

--- python_code/bolton_multilayer_thinfilm.py
+++ python_code/bolton_multilayer_thinfilm.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------- #
 #
-#    Copyright (C) 2023 Jake Kurtz
+#    Copyright (C) 2024 Jake Kurtz
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
